ffadee000d1f66c781fda721286bd1d8140efe57
IamMarkReyRonolo <ronolomarkrey@gamil.com>
2021-04-30 19:40:16 +0800

    created model for Student

---
+++ src/models/Student.js
@@ -0,0 +1,27 @@
+const sequelize = require("sequelize");
+const database = require("../database/database");
+
+const Student = database.define(
+	"Student",
+	{
+		first_name: {
+			type: sequelize.STRING,
+			allowNull: false,
+		},
+		last_name: {
+			type: sequelize.STRING,
+			allowNull: false,
+		},
+		course: {
+			type: sequelize.STRING,
+			allowNull: false,
+		},
+		year: {
+			type: sequelize.NUMBER,
+			allowNull: false,
+		},
+	},
+	{ tableName: "Student" }
+);
+
+module.exports = Student;
